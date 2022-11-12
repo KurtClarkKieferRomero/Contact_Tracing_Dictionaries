3e49778a786513f1fd140ebbeff47239cf623a06
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-11-12 18:59:44 +0800

    Introduction to the program

---
+++ understandingDictionaryList.py
@@ -0,0 +1,12 @@
+# Introduce the program
+print("Welcome to Contract Tracing program")
+print("============================================================")
+print("Listed below is the menu of the program")
+print("============================================================")
+# Create a menu
+# Ask for the user input
+# Establish the choices in the menu
+# create choice 1
+# create choice 2
+# create a looping exit or retry
+# AMOGUS
