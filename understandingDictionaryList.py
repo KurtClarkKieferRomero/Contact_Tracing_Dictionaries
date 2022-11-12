d47ec42f641690a9ab26d84732fc9f8594135502
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-11-12 19:43:29 +0800

    Added inputs for contact information

--- understandingDictionaryList.py
+++ understandingDictionaryList.py
@@ -13,11 +13,26 @@ print("\t3. Exit")
 print("============================================================")
 
 # Ask for the user input
-menu_choice = input("\tWhat is your choice? ")
-print("============================================================")
+menu_choice = int(input("\tWhat is your choice? "))
 
 # Establish the choices in the menu
 # create choice 1
+
+if menu_choice == 1:
+    print("============================================================")
+    print("Input the correct information below")
+    print("============================================================")
+    #name, number, gender, birthdate, email address, City/municipality, Province, Country
+    input("Name (Last name, First Name): ")
+    input("Number (Use 09******286 format): ")
+    input("Gender (Male, Female, Rather not say): ")
+    input("Birthdate (MM/DD/YYYY): ")
+    input("Email Address (sampleemail@gmail.com): ")
+    input("City/Municipality: ")
+    input("Province: ")
+    input("Country: ")
+
 # create choice 2
 # create a looping exit or retry
 # AMOGUS
+
