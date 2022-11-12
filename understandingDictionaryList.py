188236da5f8e6d879ceb03ef2d8d92da3ce31365
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-11-12 19:01:47 +0800

    Basic Menu

--- understandingDictionaryList.py
+++ understandingDictionaryList.py
@@ -3,7 +3,13 @@ print("Welcome to Contract Tracing program")
 print("============================================================")
 print("Listed below is the menu of the program")
 print("============================================================")
+
 # Create a menu
+print("Please Enter only numbers")
+print("1. Add Contacts")
+print("2. Search Contacts")
+print("3. Exit")
+
 # Ask for the user input
 # Establish the choices in the menu
 # create choice 1
